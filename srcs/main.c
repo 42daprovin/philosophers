be7383fb1ef8aceedd1cad0c5089f57e813167f6
daprovin <daprovin@e1r6p1.42.fr>
2021-07-24 01:17:19 +0200

    philo chulos

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: daprovin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/15 18:59:13 by daprovin          #+#    #+#             */
-/*   Updated: 2021/07/23 20:31:00 by daprovin         ###   ########.fr       */
+/*   Updated: 2021/07/24 01:11:11 by daprovin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,37 @@ void	*philosophers(void *v_philo)
 
 	philo = (t_philo *)v_philo;
 	if (philo->p_id % 2 == 0)
-		usleep(100);
-	while (1)
+		usleep(500);
+	while (g_data.death == 0)
 	{
-		pthread_mutex_lock(&g_data.fork[philo->p_id - 1]);
+		if (g_data.death)
+			break;
+		pthread_mutex_lock(&g_data.fork[(philo->p_id % g_data.philos) - 1]);
 		printf("%ld %d has taken a fork\n", conv_time(&g_data.tp, &g_data.tzp) - g_data.time, philo->p_id);
-		if (philo->p_id != g_data.philos)
-			pthread_mutex_lock(&g_data.fork[philo->p_id]);
-		else
-			pthread_mutex_lock(&g_data.fork[0]);
+		pthread_mutex_lock(&g_data.fork[philo->p_id % g_data.philos]);
 		printf("%ld %d has taken a fork\n", conv_time(&g_data.tp, &g_data.tzp) - g_data.time, philo->p_id);
+		if (g_data.death)
+		{
+			pthread_mutex_unlock(&g_data.fork[(philo->p_id % g_data.philos) - 1]);
+			pthread_mutex_unlock(&g_data.fork[philo->p_id % g_data.philos]);
+			break;
+		}
 		philo->last_eat = conv_time(&g_data.tp, &g_data.tzp);
 		printf("%ld %d is eating\n", philo->last_eat - g_data.time, philo->p_id);
 		ft_usleep(g_data.t_to_eat, philo->last_eat, &g_data.tp, &g_data.tzp);
-		pthread_mutex_unlock(&g_data.fork[philo->p_id - 1]);
-		if (philo->p_id != g_data.philos)
-			pthread_mutex_unlock(&g_data.fork[philo->p_id]);
-		else
-			pthread_mutex_unlock(&g_data.fork[0]);	
+		pthread_mutex_unlock(&g_data.fork[(philo->p_id % g_data.philos) - 1]);
+		pthread_mutex_unlock(&g_data.fork[philo->p_id % g_data.philos]);
 		philo->last_sleep = conv_time(&g_data.tp, &g_data.tzp);
+		if (g_data.death)
+			break;
 		printf("%ld %d is sleeping\n", philo->last_sleep - g_data.time, philo->p_id);
 		ft_usleep(g_data.t_to_sleep, philo->last_sleep, &g_data.tp, &g_data.tzp);
+		if (g_data.death)
+			break;
 		printf("%ld %d is thinking\n", conv_time(&g_data.tp, &g_data.tzp) - g_data.time, philo->p_id);
 	}
-		pthread_exit(NULL);
+	return (NULL);
+	/* pthread_exit(NULL); */
 }
 
 void	init_data(char **av, int ac)
@@ -74,6 +81,7 @@ void	init_philo(void)
 {
 	t_philo	philo[g_data.philos];
 	int	i;
+	long last_eat;
 	
 	i = 0;
 	while (i < g_data.philos)
@@ -81,7 +89,7 @@ void	init_philo(void)
 		philo[i].p_id = i + 1;
 		philo[i].last_eat = 0;
 		philo[i].t_eat = 0;
-		pthread_create(&(philo[i].t_id), NULL, philosophers, (void *)&philo[i]);	
+		pthread_create(&(philo[i].t_id), NULL, philosophers, (void *)&philo[i]);
 		i++;
 	}
 	while (g_data.death == 0)
@@ -89,14 +97,30 @@ void	init_philo(void)
 	 	i = 0;
 		while (i < g_data.philos)
 		{
-			if (philo[i].last_eat - conv_time(&g_data.tp, &g_data.tzp) >= g_data.t_to_die)
+			/* printf("t_id -----------> %d\n", philo[i].p_id); */
+			long jaume;
+			last_eat = philo[i].last_eat;
+			if ((jaume = conv_time(&g_data.tp, &g_data.tzp)) - last_eat >= g_data.t_to_die && last_eat != 0)
 			{
+				printf("ts = %ld, last_eat = %ld, to_die = %ld\n", jaume, last_eat, g_data.t_to_die);
 				g_data.death = 1;
 				printf("%ld %d died\n", conv_time(&g_data.tp, &g_data.tzp) - g_data.time, i + 1);
+				/* i = 0; */
+				/* while (i < g_data.philos) */
+				/* { */
+				/* 	pthread_detach(philo[i].t_id); */
+				/* 	i++; */
+				/* } */
 				break ;
 			}
 			i++;
 		}	
+	}
+	i = 0;
+	while (i < g_data.philos)
+	{
+		pthread_join(philo[i].t_id, NULL);
+		i++;
 	}
 }
 
